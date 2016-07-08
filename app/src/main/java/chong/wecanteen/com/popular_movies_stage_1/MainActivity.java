e3043db83618b6412cc7ccb5e7342f3afd56031a
Wang Chong <wccake@gmail.com>
2016-07-08 10:20:11 +0800

    Load fake image in GridView with GridViewAdapter

--- app/src/main/java/chong/wecanteen/com/popular_movies_stage_1/MainActivity.java
+++ app/src/main/java/chong/wecanteen/com/popular_movies_stage_1/MainActivity.java
@@ -1,27 +1,32 @@
 package chong.wecanteen.com.popular_movies_stage_1;
 
+import android.os.Bundle;
 import android.os.Handler;
 import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.widget.GridView;
 import android.widget.Toast;
+
+import chong.wecanteen.com.popular_movies_stage_1.adapter.GridViewAdapter;
 
 public class MainActivity extends AppCompatActivity implements SwipeRefreshLayout.OnRefreshListener {
     public static final String TAG = MainActivity.class.getSimpleName();
 
     private SwipeRefreshLayout mSwipeRefreshLayout;
     private GridView mGridView;
-
+    private GridViewAdapter adapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         mSwipeRefreshLayout = (SwipeRefreshLayout) findViewById(R.id.container);
-        mGridView = (GridView) findViewById(R.id.movie_gridview);
-
         mSwipeRefreshLayout.setOnRefreshListener(MainActivity.this);
+
+        mGridView = (GridView) findViewById(R.id.movie_gridview);
+        adapter = new GridViewAdapter(this);
+
+        mGridView.setAdapter(adapter);
     }
 
     @Override
@@ -35,4 +40,6 @@ public class MainActivity extends AppCompatActivity implements SwipeRefreshLayou
         }, 2000);
         mSwipeRefreshLayout.setColorSchemeResources(R.color.blue, R.color.purple, R.color.green, R.color.orange);
     }
+
+
 }
